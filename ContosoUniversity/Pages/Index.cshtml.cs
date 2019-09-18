7750f03f9f698f9ffe8ac7ad8129346a7d14cffa
Julian <juli11c0@edu.eal.dk>
2019-09-18 20:10:23 +0200

    det jeg ha rlavet i skolen med jimmy

--- ContosoUniversity/Pages/Index.cshtml.cs
+++ ContosoUniversity/Pages/Index.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ContosoUniversity.Models.SchoolContext _context;
 
+        public IndexModel(ContosoUniversity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
         }
     }
 }
